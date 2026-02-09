14f97edd989a2ea0db40c84c2eabcb41dc2ad48e
Dorian CARDOSO <doriancardoso00@gmail.com>
2026-02-09 11:55:19 +0100

    feat: show only one project detail opened at a time

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const projects = document.querySelectorAll(".project");
 
 projects.forEach((project) => {
-    const title = project.querySelector(".project-title");
+  const title = project.querySelector(".project-title");
 
-    title.addEventListener("click", () => {
-        project.classList.toggle("active");
+  title.addEventListener("click", () => {
+    const isActive = project.classList.contains("active");
+
+    projects.forEach((p) => {
+      p.classList.remove("active");
     });
-})
+
+    if (!isActive) {
+      project.classList.add("active");
+    }
+  });
+});
